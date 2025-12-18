9e98abf5e9b947100b7234040a7b6a498a220396
Paiva <danpaiva@live.com>
2025-12-18 20:19:00 -0300

    feat: criado metodos no ProductRepository

--- RepositoryStoreApi/RepositoryStoreApi/Repositories/ProductRepository.cs
+++ RepositoryStoreApi/RepositoryStoreApi/Repositories/ProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using RepositoryStoreApi.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryStoreApi.Data;
 using RepositoryStoreApi.Models;
 
 namespace RepositoryStoreApi.Repositories;
@@ -11,5 +12,37 @@ public class ProductRepository(AppDbContext context)
         await context.SaveChangesAsync(cancellationToken);
 
         return product;
+    }
+
+    public async Task<Product> UpdateAsync(Product product, CancellationToken cancellationToken = default)
+    {
+        context.Products.Update(product);
+        await context.SaveChangesAsync(cancellationToken);
+
+        return product;
+    }
+
+    public async Task DeleteAsync(Product product, CancellationToken cancellationToken = default)
+    {
+        context.Products.Remove(product);
+        await context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task<Product?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+    {
+        return await context
+            .Products
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+    }
+
+    public async Task<List<Product>> GetAllAsync(int skip = 0, int take = 25, CancellationToken cancellationToken = default)
+    {
+        return await context
+            .Products
+            .AsNoTracking()
+            .Skip(skip)
+            .Take(take)
+            .ToListAsync(cancellationToken);
     }
 }
