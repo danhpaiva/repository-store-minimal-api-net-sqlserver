3e27efd5a34b2be91a8b9466bfcd02ad0a47f39c
Paiva <danpaiva@live.com>
2025-12-18 20:27:02 -0300

    feat: criar interface IProductRepository

---
+++ RepositoryStoreApi/RepositoryStoreApi/Repositories/Abstractions/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using RepositoryStoreApi.Models;
+
+namespace RepositoryStoreApi.Repositories.Abstractions;
+
+public interface IProductRepository
+{
+    Task<Product> CreateAsync(Product product, CancellationToken cancellationToken = default);
+    Task<Product> UpdateAsync(Product product, CancellationToken cancellationToken = default);
+    Task DeleteAsync(Product product, CancellationToken cancellationToken = default);
+    Task<Product?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+}
