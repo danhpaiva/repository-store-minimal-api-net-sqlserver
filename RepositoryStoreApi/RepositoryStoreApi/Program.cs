949abb907f0c75b30708e41801762914385473b6
Paiva <danpaiva@live.com>
2025-12-18 19:50:40 -0300

    feat: add ConnectString no Program.cs

--- RepositoryStoreApi/RepositoryStoreApi/Program.cs
+++ RepositoryStoreApi/RepositoryStoreApi/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using RepositoryStoreApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddOpenApi();
 
