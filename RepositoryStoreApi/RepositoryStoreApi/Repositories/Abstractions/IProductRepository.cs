b3f5310e32bc49670253584a9abc43f83919ab0c
Paiva <danpaiva@live.com>
2025-12-18 20:27:38 -0300

    feat: add metodo GetAllAsync na interface

--- RepositoryStoreApi/RepositoryStoreApi/Repositories/Abstractions/IProductRepository.cs
+++ RepositoryStoreApi/RepositoryStoreApi/Repositories/Abstractions/IProductRepository.cs
@@ -8,4 +8,5 @@ public interface IProductRepository
     Task<Product> UpdateAsync(Product product, CancellationToken cancellationToken = default);
     Task DeleteAsync(Product product, CancellationToken cancellationToken = default);
     Task<Product?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+    Task<List<Product>> GetAllAsync(int skip = 0, int take = 25, CancellationToken cancellationToken = default);
 }
