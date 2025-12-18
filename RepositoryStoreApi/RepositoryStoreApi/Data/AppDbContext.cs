c80b6568e1bcf275101f32aa58e40ab048a693d2
Paiva <danpaiva@live.com>
2025-12-18 19:40:17 -0300

    feat: add criacao da classe AppDbContext para mapear a tabela Produtos

---
+++ RepositoryStoreApi/RepositoryStoreApi/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryStoreApi.Models;
+
+namespace RepositoryStoreApi.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Product> Products { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        => modelBuilder.ApplyConfigurationsFromAssembly(typeof(Program).Assembly);
+}
