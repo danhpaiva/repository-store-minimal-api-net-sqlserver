b543694da29d224807f47db35fb9bd003035be0b
Paiva <danpaiva@live.com>
2025-12-18 19:51:53 -0300

    feat: add primeira migracao com o EfCore

---
+++ RepositoryStoreApi/RepositoryStoreApi/Migrations/20251218225135_PrimeiraMigracao.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RepositoryStoreApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class PrimeiraMigracao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "NVARCHAR(160)", maxLength: 160, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
