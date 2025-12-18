08479586535d174c12e1a4f0d6d87203996f2963
Paiva <danpaiva@live.com>
2025-12-18 19:44:43 -0300

    feat: add ProductMap para regras de tabela de banco de dados

---
+++ RepositoryStoreApi/RepositoryStoreApi/Data/Mapping/ProductMap.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RepositoryStoreApi.Models;
+
+namespace RepositoryStoreApi.Data.Mapping;
+
+public class ProductMap : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.ToTable("Products");
+
+        builder.HasKey(p => p.Id);
+
+        builder.Property(p => p.Title)
+            .IsRequired(true)
+            .HasMaxLength(160)
+            .HasColumnType("NVARCHAR");
+    }
+}
